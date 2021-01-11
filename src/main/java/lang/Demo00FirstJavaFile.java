df524323e47f5292f8ba759b908a60fa535faedb
Bernhard Wallisch <bernhard_wallisch@hotmail.com>
2021-01-11 17:23:29 +0100

    added doc-chapter "1 Java Grundgerüst"

---
+++ src/main/java/lang/Demo00FirstJavaFile.java
@@ -0,0 +1,10 @@
+package lang;                                   // Package-Name, the "subdirectory" where the Java-file is stored
+
+public class Demo00FirstJavaFile                // Class-Name, must be equal to the Filename of the .java-file (without extension)
+{
+    public static void main(String[] args) {    // The main-program
+        // the contained code here will be executed when the Program is started
+
+        System.out.println("This is my first java program");    // prints a text (on the console)
+    }
+}
